743e9576c3b5ce104b0387aafe797b897173afe7
ZaidTariqShah <138977854+ZaidTariqShah@users.noreply.github.com>
2026-07-16 22:19:16 +0530

    Add function to count unique digits in a number
    Implement function to count digits that appear exactly once in a number.

---
+++ countDigitsThatAppearExactlyOnce.js
@@ -0,0 +1,32 @@
+let num = 0;
+let original = num;
+let count = 0;
+
+if (num === 0) {
+    count = 1;
+} else {
+    while (num > 0) {
+        let track = 0;
+        let check = num % 10;
+
+        let duplicate = original;
+
+        while (duplicate > 0) {
+            let check2 = duplicate % 10;
+
+            if (check === check2) {
+                track++;
+            }
+
+            duplicate = Math.floor(duplicate / 10);
+        }
+
+        if (track === 1) {
+            count++;
+        }
+
+        num = Math.floor(num / 10);
+    }
+}
+
+console.log(count);
