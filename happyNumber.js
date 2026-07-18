12237615b2156abf019ba0c977e85c9ed4c81fe4
ZaidTariqShah <138977854+ZaidTariqShah@users.noreply.github.com>
2026-07-18 21:36:41 +0530

    Refactor happy number logic without new set

--- happyNumber.js
+++ happyNumber.js
@@ -22,3 +22,37 @@ if (num === 1) {
 } else {
     console.log("It is not a Happy Number");
 }
+
+/// WITHOUT NEW SET
+let num = Number(prompt("Please write a number"));
+    while(num !== 1 && num !== 4) {
+    let sum = 0;
+    while(num > 0) {
+    let digit = num % 10;
+        sum = sum + digit ** 2;
+        num = Math.floor(num/10);
+}
+        num = sum;
+    }
+if(num === 1) {
+    console.log(`It is a Happy Number`)
+} else {
+    console.log(`It is not a Happy Number`)
+}
+VM87:12 It is a Happy Number
+undefined
+let num = Number(prompt("Please write a number"));
+    while(num !== 1 && num !== 4) {
+    let sum = 0;
+    while(num > 0) {
+    let digit = num % 10;
+        sum = sum + digit ** 2;
+        num = Math.floor(num/10);
+}
+        num = sum;
+    }
+if(num === 1) {
+    console.log(`It is a Happy Number`)
+} else {
+    console.log(`It is not a Happy Number`)
+}
