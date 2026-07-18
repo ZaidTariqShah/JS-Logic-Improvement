af794acce3b7de62fd3154cdd6a0c372f6a61905
ZaidTariqShah <138977854+ZaidTariqShah@users.noreply.github.com>
2026-07-18 13:05:14 +0530

    Optimize prime number detection algorithm
    Added optimization to prime number checking by reducing checks for even and multiples of 3, and implementing a 6k ± 1 optimization.

--- list-primes.js
+++ list-primes.js
@@ -19,3 +19,43 @@ if (num <= 1) {
         }
     }
 }
+
+
+//More OPTIMIZATION
+let num = Number(prompt("Please enter a number"));
+
+if (num <= 1) {
+    alert("The number must be greater than 1.");
+} else {
+    for (let i = 2; i <= num; i++) {
+        let isPrime = true;
+
+        if (i > 2 && i % 2 === 0) {
+            isPrime = false;
+        }
+
+        if (i > 3 && i % 3 === 0) {
+            isPrime = false;
+        }
+
+        let limit = Math.sqrt(i);
+
+        if (isPrime) {
+            for (let j = 1; (6 * j - 1) <= limit; j++) {
+                if (i % (6 * j + 1) === 0) {
+                    isPrime = false;
+                    break;
+                }
+
+                if (i % (6 * j - 1) === 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+        }
+
+        if (isPrime) {
+            console.log(i);
+        }
+    }
+}
