482678ac5a6bc6fed36d87baa16ff44735e1c935
ZaidTariqShah <138977854+ZaidTariqShah@users.noreply.github.com>
2026-07-19 12:41:57 +0530

    Add strong number validation in strongNumber.js
    Implement strong number check based on user input.

---
+++ strongNumber.js
@@ -0,0 +1,27 @@
+let num = Number(prompt("Please write a number"));
+let sum = 0;
+
+if (isNaN(num) || num < 0) {
+    alert("Please write a proper number");
+} else {
+    let nextNum = num;
+
+    do {
+        let digit = nextNum % 10;
+        let factorial = 1;
+
+        for (let i = 1; i <= digit; i++) {
+            factorial *= i;
+        }
+
+        sum += factorial;
+        nextNum = Math.floor(nextNum / 10);
+
+    } while (nextNum > 0);
+
+    console.log(
+        num === sum
+            ? `${num} is a Strong Number`
+            : `${num} is not a Strong Number`
+    );
+}
