f5312de9e8585073ea9c495d6e1a717df18fdf61
ZaidTariqShah <138977854+ZaidTariqShah@users.noreply.github.com>
2026-07-19 16:26:33 +0530

    Create mysteryNumber.js

---
+++ mysteryNumber.js
@@ -0,0 +1,16 @@
+let num = 123456;
+let sum = 0;
+
+while (num > 0) {
+    let digit = num % 10;
+
+    if (digit % 2 !== 0) {
+        sum = sum + digit * 2;
+    } else {
+        sum = sum + digit;
+    }
+
+    num = Math.floor(num / 10);
+}
+
+console.log(sum);
