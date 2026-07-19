5d3df2495328cddd3c63b6ad034bb2d6adef05e9
ZaidTariqShah <138977854+ZaidTariqShah@users.noreply.github.com>
2026-07-19 23:14:47 +0530

    Update fibonacciSeries.js

--- fibonacciSeries.js
+++ fibonacciSeries.js
@@ -1,16 +1,13 @@
 let num = Number(prompt("Please enter a number"));
-
-if (isNaN(num) || num < 0) {
-    alert("Please write a proper number");
+if(isNaN(num) || num < 0) {
+alert("Please write a proper number")
 } else {
-    let a = 0;
-    let b = 1;
-
-    while (a <= num) {
-        console.log(a);
-
-        let next = a + b;
-        a = b;
-        b = next;
-    }
+let a = 0;
+let b = 1;
+while(a <= num) {
+console.log(a);
+let next = a + b;
+a = b;
+b = next;
+}
 }
