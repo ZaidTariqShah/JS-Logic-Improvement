702ec086a9c140e90eba6b809dfb4e4827b054b6
ZaidTariqShah <138977854+ZaidTariqShah@users.noreply.github.com>
2026-07-21 13:30:49 +0530

    Add function to find the smallest element in an array

---
+++ Arrays/smallestElement.js
@@ -0,0 +1,15 @@
+let arr = [1, 2, 3, -4, 5];
+
+if (arr.length === 0) {
+    alert("Array is empty");
+} else {
+    let min = arr[0];
+
+    for (let i = 1; i < arr.length; i++) {
+        if (min > arr[i]) {
+            min = arr[i];
+        }
+    }
+
+    console.log(min);
+}
