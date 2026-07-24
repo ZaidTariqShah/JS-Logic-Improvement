c4c67fd099128ac10a3602036104489f8f60f08d
ZaidTariqShah <138977854+ZaidTariqShah@users.noreply.github.com>
2026-07-24 16:14:01 +0530

    Update arrayDistinctElementFrequency.js

--- Arrays/arrayDistinctElementFrequency.js
+++ Arrays/arrayDistinctElementFrequency.js
@@ -20,3 +20,24 @@ for (let i = 0; i < arr.length; i++) {
         console.log(`${arr[i]} appeared ${count} times`);
     }
 }
+
+// OPTIMIZED APPROACH
+let arr = [1,2,3,4,5,6,7,7,8,6,9,10];
+for(let i = 0; i < arr.length; i++) {
+    let track = 0;
+    for(let j = i - 1; j >= 0; j--) {
+        if(arr[i] === arr[j]) {
+            track++;
+            break;
+        }
+    }
+    if(track === 0) {
+        let count = 1; // Because we are already standing on this element.
+        for(let k = i + 1; k < arr.length; k++) { // Check only the remaining elements after the current one.
+            if(arr[i] === arr[k]) {
+                count++;
+            }
+        }
+        console.log(`${arr[i]} appeared ${count} times`);
+    }
+}
