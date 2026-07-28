598f4c0a6862d3bf58ea16a61b644a22fd8bb42b
ZaidTariqShah <138977854+ZaidTariqShah@users.noreply.github.com>
2026-07-28 14:58:21 +0530

    Add zero solver for descending order of digits
    Implemented a zero solver to sort digits in descending order.

--- descending-order.js
+++ descending-order.js
@@ -36,3 +36,59 @@ while (num) {
 }
 
 console.log(result);
+
+                                            //ZERO SOLVER VERSION HERE
+
+
+let num = 123589700;
+
+let temp = num;
+let digitsCount = 0;
+
+while (temp) {
+    digitsCount++;
+    temp = Math.floor(temp / 10);
+}
+
+let result = 0;
+
+while (digitsCount) {
+    let max = 0;
+    let temp = num;
+    let count = digitsCount;
+
+    while (count) {
+        let check = temp % 10;
+
+        if (check > max) {
+            max = check;
+        }
+
+        temp = Math.floor(temp / 10);
+        count--;
+    }
+
+    result = result * 10 + max;
+
+    let newNum = 0;
+    let isRemoved = false;
+    let count1 = digitsCount;
+
+    while (count1) {
+        let check = num % 10;
+
+        if (check === max && !isRemoved) {
+            isRemoved = true;
+        } else {
+            newNum = newNum * 10 + check;
+        }
+
+        num = Math.floor(num / 10);
+        count1--;
+    }
+
+    num = newNum;
+    digitsCount--;
+}
+
+console.log(result);
