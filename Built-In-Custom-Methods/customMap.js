1760c5ee44cba480af9c8af90be64fb7e2b12c4f
ZaidTariqShah <138977854+ZaidTariqShah@users.noreply.github.com>
2026-07-31 23:07:08 +0530

    Refactor custom map function implementation

--- Built-In-Custom-Methods/customMap.js
+++ Built-In-Custom-Methods/customMap.js
@@ -30,14 +30,19 @@ console.log(result);
 
 //---------------------------------------------------------Easy VERSION-------------------------------------------------------------------
 let array = [1,2,3,4,5,6,7,8,9,10];
+
 function map(cb) {
     let newArray = [];
-    for(let i = 0; i < array.length; i++) {
-        newArray.push(cb(array[i],i,array));
+
+    for (let i = 0; i < array.length; i++) {
+        newArray.push(cb(array[i], i, array));
     }
+
     return newArray;
-    }
-const result = array.map(function(i) {
+}
+
+const result = map(function(i) {
     return i * 2;
-})
+});
+
 console.log(result);
