9c1e9bc6696e85432911e6cf3e87d95cd95429c3
ZaidTariqShah <138977854+ZaidTariqShah@users.noreply.github.com>
2026-08-02 21:20:09 +0530

    Rename parameter in unshift function
    Renamed parameter 'element' to 'elements' for clarity.

---
+++ Built-In-Custom-Methods/custom-unshift.js
@@ -0,0 +1,19 @@
+let arr = [1, 2, 3, 4, 5];
+
+function unshift(...elements) {
+    let shiftBy = element.length;
+
+    for (let i = arr.length - 1; i >= 0; i--) {
+        arr[i + shiftBy] = arr[i];
+    }
+
+    for (let i = 0; i < element.length; i++) {
+        arr[i] = element[i];
+    }
+
+    return arr.length;
+}
+
+const size = unshift(10, 20, 30);
+
+console.log(arr, size);
