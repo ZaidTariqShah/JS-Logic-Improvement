4335d63a3f37cadb636440b22db41ccaf0d58c8e
ZaidTariqShah <138977854+ZaidTariqShah@users.noreply.github.com>
2026-08-19 11:36:35 +0530

    Create ReflectProxy.js
    Reflect is a built-in JavaScript object that provides methods for performing JavaScript's standard operations on objects programmatically.

---
+++ proxy-property-counterproxy /ReflectProxy.js
@@ -0,0 +1,24 @@
+let target = {
+    length: 0
+};
+
+let handleObject = {
+    set(target, property, value) {
+
+        Reflect.set(target, property, value);
+
+        target.length = Object.keys(target).length - 1;
+
+        return true;
+    }
+};
+
+let proxy = new Proxy(target, handleObject);
+
+proxy.name = "test";
+
+console.log(proxy);
+
+proxy.name = "test2";
+
+console.log(proxy);
