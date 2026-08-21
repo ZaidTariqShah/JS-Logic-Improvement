ab4479cc2cee711c0f9bf9a466b548d32d3a1485
ZaidTariqShah <138977854+ZaidTariqShah@users.noreply.github.com>
2026-08-21 12:43:26 +0530

    Implement computer player logic in tic-tac-toe
    Added computer player functionality with a delay for moves and improved reset logic.

--- tic-tac-toe/script.js
+++ tic-tac-toe/script.js
@@ -4,6 +4,8 @@ let player = "X";
 let para = document.querySelector("#para");
 let isGameOver = false;
 let reset = document.querySelector("#reset");
+let computerTimer;
+let computersThinking = false;
 
 let winningCombinations = [
   [0, 1, 2],
@@ -22,6 +24,7 @@ let buttons = boardElement.querySelectorAll("button");
 let arrayofButtons = Array.from(buttons);
 arrayofButtons.forEach((button) => {
   button.addEventListener("click", function (e) {
+    if (computersThinking) return;
     if (isGameOver) return;
     let clickedIndex = arrayofButtons.indexOf(e.target);
     if (board[clickedIndex] !== "") return; //click extra
@@ -32,20 +35,31 @@ arrayofButtons.forEach((button) => {
     checkBoardFull();
     if (isGameOver) return;
     switchPlayer();
+    let computersIndex = computerMove();
+    computerTimer = setTimeout(() => {
+      board[computersIndex] = player;
+      arrayofButtons[computersIndex].innerText = player;
+      checkWinner();
+      if (isGameOver) return;
+      switchPlayer();
+      checkBoardFull();
+      computersThinking = false;
+    }, 1000);
   });
 });
 
 function checkWinner() {
-  winningCombinations.forEach((combination) => {
+  for (let combination of winningCombinations) {
     if (
       board[combination[0]] === player &&
       board[combination[1]] === player &&
       board[combination[2]] === player
     ) {
-      para.innerText = `${player} WINS`;
+      para.innerHTML = `${player} Wins`;
       isGameOver = true;
+      break;
     }
-  });
+  }
 }
 
 function switchPlayer() {
@@ -68,6 +82,8 @@ function checkBoardFull() {
 }
 
 reset.addEventListener("click", function () {
+  clearTimeout(computerTimer); // returns id and clears Time out from that ID
+  computersThinking = false;
   arrayofButtons.forEach((button) => {
     button.innerText = "";
   });
@@ -76,3 +92,18 @@ reset.addEventListener("click", function () {
   para.innerText = "";
   board.fill("");
 });
+
+let emptySpaces = [];
+function computerMove() {
+  computersThinking = true;
+  emptySpaces = [];
+  for (let i = 0; i < board.length; i++) {
+    if (board[i] === "") {
+      emptySpaces.push(i);
+    }
+  }
+  let computersIndex =
+    emptySpaces[Math.floor(Math.random() * emptySpaces.length)];
+  // board[computersIndex] = player;
+  return computersIndex;
+}
